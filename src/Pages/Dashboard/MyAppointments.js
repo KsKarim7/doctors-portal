0db55fe77a9ae53c65a2dad8a6e1c3ae7988d32f
Khaled Karim <kskarim177@gmail.com>
2022-05-15 20:56:05 +0600

    appointment data of individual patient is loaded form backend

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppointments = () => {
+    const [appointments, setAppointments] = useState([]);
+    const [user] = useAuthState(auth);
+
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointments(data))
+        }
+    }, [user])
     return (
         <div>
-            <h2>My Appointments</h2>
+            <h2>My Appointments; {appointments.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
